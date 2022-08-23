c48f31c2ffdc4d2fa16a82dedd3340e106fc764c
hmy343 <57212274+hmy343@users.noreply.github.com>
2022-08-23 17:55:46 +0900

    Revert "Hyunmin"

--- src/hooks/useBooks.js
+++
@@ -1,8 +0,0 @@
-
-import { useContext } from 'react';
-import AppStateContext from '../contexts/AppStateConText';
-
-export default function useBooks() {
-  const { books, searchBook } = useContext(AppStateContext);
-  return { books, searchBook } 
-}
--- src/hooks/useNewest.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import AppStateContext from '../contexts/AppStateConText';
-
-
-export default function useNewest() {
-  const { handleNewest, handleHighPrice, handleLowPrice } = useContext(AppStateContext);
-  return { handleNewest, handleHighPrice, handleLowPrice } 
-};
