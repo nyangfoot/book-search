e5ad7a782c94e4d6e2e6f37ff5bd40664cf526d8
min <hmy343@naver.com>
2022-08-23 16:16:44 +0900

    add router and book detail

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './App.css';
 import MainPage from './page/MainPage';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import AppStateProvider from './providers/AppStateProvider';
+import BookDetail from './page/BookDetail';
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
-      <AppStateProvider>
+      {/* <AppStateProvider>
         <MainPage />
-      </AppStateProvider>
+      </AppStateProvider> */}
+      <Routes>
+        <Route path='/' element={<AppStateProvider>
+          <MainPage />
+        </AppStateProvider>} />
+        <Route path='/book-detail' element={<AppStateProvider>
+          <BookDetail />
+        </AppStateProvider>} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
