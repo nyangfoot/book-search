be0ced6d61ed459548d95a18bfdc0484c33b3fea
min <hmy343@gmail.com>
2022-09-05 22:27:34 +0900

    update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  // getByText로 가져오는 글자가 해당 파일에 존재하는지를 체크하는 것이므로
+  // const linkElement = screen.getByText(/learn react/i);
+  // 그래서 메인 화면에 뜨게 되는 아무 글자를 넣어줌!
+  const linkElement = screen.getByText(/Book Search/);
   expect(linkElement).toBeInTheDocument();
 });
