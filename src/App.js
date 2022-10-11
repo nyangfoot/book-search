d9cabcfa96999648fba2896b9177bbf6eb77ef6f
min <hmy343@gmail.com>
2022-10-11 15:06:35 +0900

    router baseurl setting update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import BookDetail from './page/BookDetail';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <div>
       <Routes>
         <Route path='/' element={<AppStateProvider>
